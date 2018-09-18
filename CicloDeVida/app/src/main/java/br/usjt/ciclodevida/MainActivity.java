7b4a52066de2fea70e8929f2d907da3455c31fbc
renan <renan.gds97@gmail.com>
2018-09-18 00:12:02 -0300

    Aula - Ciclo de Vida

---
+++ CicloDeVida/app/src/main/java/br/usjt/ciclodevida/MainActivity.java
@@ -0,0 +1,18 @@
+package br.usjt.ciclodevida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+* RENAN GOUVEIA DUTRA DA SILVA
+* 816114946
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
